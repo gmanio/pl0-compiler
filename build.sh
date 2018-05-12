1943892b0d53429e6e2097893fefcfbe0c8d7f66
JimanPark <jiman.park@sk.com>
2018-05-12 12:17:42 +0900

    [feature] README.md

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-flex parser.l
-gcc -o compiler lex.yy.c -ll -std=gnu89
-#./compiler < input.l
--- pl01.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-flex pl01.l
-
-gcc -o pl01 lex.yy.c -ll -std=gnu89
-
-./pl01 < input.l
